572faf5ade7eee5c58c14be2c3daa6ece2a6dc31
lep13 <lepakshi1304@gmail.com>
2024-05-22 11:49:48 +0530

    public repo working

--- main.go
+++ main.go
@@ -5,29 +5,43 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/lep13/AutoBuildGo/cmd/api"
+	"github.com/lep13/AutoBuildGo/pkg/services/Repo"
 	"github.com/lep13/AutoBuildGo/pkg/services/ecr"
 )
 
-const (
-	host = "localhost"
-	port = "8080"
-)
+// const (
+//  host = "localhost"
+//  port = "8080"
+// )
 
 func main() {
+	token := flag.String("token", "", "GitHub personal access token")
+	gitrepoName := flag.String("git-repo-name", "", "Name of the GitHub repository")
+	description := flag.String("description", "", "Description of the GitHub repository")
+	isPrivate := flag.Bool("private", false, "Make the GitHub repository private")
+
 	createRepo := flag.Bool("create-repo", false, "Create ECR repository")
 	pushImage := flag.Bool("push-image", false, "Push Docker image to ECR repository")
-	region := flag.String("region", "ap-south-1", "AWS region")
+	region := flag.String("region", "us-east-1", "AWS region")
 	repoName := flag.String("repo-name", "ecr-repo-1", "ECR repository name")
 	imageTag := flag.String("image-tag", "latest", "Docker image tag")
 
 	flag.Parse()
+	if *token != "" && *gitrepoName != "" && *description != "" {
+		// Call the function to create the GitHub repository
+		err := Repo.CreateGitHubRepo(*token, *gitrepoName, *description, *isPrivate)
+		if err != nil {
+			log.Fatal("Error creating GitHub repository:", err)
+		}
+
+		fmt.Println("GitHub repository created successfully!")
+	}
 
 	if *createRepo {
 		if *repoName == "" {
 			log.Fatal("repository name is required")
 		}
-		ecr.CreateRepository(*region, *repoName)
+		ecr.CreatePublicRepository(*region, *repoName)
 		return
 	}
 
@@ -42,10 +56,5 @@ func main() {
 		return
 	}
 
-	log.Println("No action specified. Use -create-repo or -push-image flag to perform the desired action.")
-
-	err := api.ServeHTTP(fmt.Sprintf("%s:%s", host, port))
-	if err != nil {
-		log.Fatalf("[ERROR]: %s", err)
-	}
+	log.Println("No action specified. Use -create-repo or -push-image  flag to perform the desired action.")
 }
--- pkg/services/ecr/ecr.go
+++ pkg/services/ecr/ecr.go
@@ -12,23 +12,24 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ecr"
-	"github.com/aws/aws-sdk-go-v2/service/ecr/types"
+	ecrpublic "github.com/aws/aws-sdk-go-v2/service/ecrpublic"
+	//  "github.com/aws/aws-sdk-go-v2/service/ecrpublic/types"
 )
 
-// CreateRepository creates a new ECR repository
-func CreateRepository(region, repoName string) {
+// CreatePublicRepository creates a new public ECR repository
+func CreatePublicRepository(region, repoName string) {
 	// Load the AWS configuration with the provided region
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
 	if err != nil {
 		log.Fatalf("unable to load SDK config, %v", err)
 	}
 
-	// Create a new ECR client with the loaded configuration
-	svc := ecr.NewFromConfig(cfg)
+	// Create a new ECR public client with the loaded configuration
+	svc := ecrpublic.NewFromConfig(cfg)
 
-	// Create the ECR repository
-	input := &ecr.CreateRepositoryInput{
-		RepositoryName: aws.String(repoName), ImageTagMutability: types.ImageTagMutabilityImmutable,
+	// Create the ECR public repository
+	input := &ecrpublic.CreateRepositoryInput{
+		RepositoryName: aws.String(repoName),
 	}
 
 	result, err := svc.CreateRepository(context.TODO(), input)
@@ -36,7 +37,8 @@ func CreateRepository(region, repoName string) {
 		log.Fatalf("failed to create repository, %v", err)
 	}
 
-	fmt.Printf("Repository %s created with URI: %s\n", *result.Repository.RepositoryName, *result.Repository.RepositoryUri)
+	repoURI := *result.Repository.RepositoryUri
+	fmt.Printf("Public repository %s created with URI: %s\n", *result.Repository.RepositoryName, repoURI)
 }
 
 // PushImageToRepository pushes a Docker image to the ECR repository
