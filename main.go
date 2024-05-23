cadc55d0dcbd8d5adc9f083a6ec4842ed05aa89e
lep13 <lepakshi1304@gmail.com>
2024-05-23 11:25:46 +0530

    Files have been modified according to the requirements discussed

--- main.go
+++ main.go
@@ -1,65 +1,16 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
 
-	"github.com/lep13/AutoBuildGo/pkg/services/Repo"
-	"github.com/lep13/AutoBuildGo/pkg/services/ecr"
-)
-
-const (
-	host = "localhost"
-	port = "8080"
+	"github.com/lep13/AutoBuildGo/ecr"
 )
 
 func main() {
-	token := flag.String("token", "", "GitHub personal access token")
-	gitrepoName := flag.String("git-repo-name", "", "Name of the GitHub repository")
-	description := flag.String("description", "", "Description of the GitHub repository")
-	isPrivate := flag.Bool("private", false, "Make the GitHub repository private")
+    err := ecr.CreateRepo()
+    if err != nil {
+        log.Fatalf("Failed to create repository: %v", err)
+    }
 
-	createRepo := flag.Bool("create-repo", false, "Create ECR repository")
-	pushImage := flag.Bool("push-image", false, "Push Docker image to ECR repository")
-	region := flag.String("region", "ap-south-1", "AWS region")
-	repoName := flag.String("repo-name", "ecr-repo-1", "ECR repository name")
-	imageTag := flag.String("image-tag", "latest", "Docker image tag")
-
-	flag.Parse()
-	if *token != "" && *gitrepoName != "" && *description != "" {
-		// Call the function to create the GitHub repository
-		err := Repo.CreateGitHubRepo(*token, *gitrepoName, *description, *isPrivate)
-		if err != nil {
-			log.Fatal("Error creating GitHub repository:", err)
-		}
-
-		fmt.Println("GitHub repository created successfully!")
-	}
-
-	if *createRepo {
-		if *repoName == "" {
-			log.Fatal("repository name is required")
-		}
-		ecr.CreateRepository(*region, *repoName)
-		return
-	}
-
-	if *pushImage {
-		if *repoName == "" || *imageTag == "" {
-			log.Fatal("both repository name and image tag are required")
-		}
-		err := ecr.PushImageToRepository(*region, *repoName, *imageTag)
-		if err != nil {
-			log.Fatalf("failed to push image to repository: %v", err)
-		}
-		return
-	}
-
-	// log.Println("No action specified. Use -create-repo or -push-image  flag to perform the desired action.")
-
-	// err1 := api.ServeHTTP(fmt.Sprintf("%s:%s", host, port))
-	// if err1 != nil {
-	// 	log.Fatalf("[ERROR]: %s", err1)
-	// }
+    log.Println("Repository created successfully")
 }
---
+++ services/ecr/repository.go
@@ -0,0 +1,95 @@
+package ecr
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	// "strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ecr"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	smTypes "github.com/aws/aws-sdk-go-v2/service/secretsmanager/types"
+	"github.com/aws/aws-sdk-go-v2/service/ecr/types"
+)
+
+type AWSCredentials struct {
+	AccessKeyID     string `json:"access_key_id"`
+	SecretAccessKey string `json:"secret_access_key"`
+	SessionToken    string `json:"session_token"`
+}
+
+func getAWSCredentials(secretName string) (*AWSCredentials, error) {
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("unable to load SDK config, %v", err)
+	}
+
+	svc := secretsmanager.NewFromConfig(cfg)
+
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(secretName),
+	}
+
+	result, err := svc.GetSecretValue(context.Background(), input)
+	if err != nil {
+		var resourceNotFoundException *smTypes.ResourceNotFoundException
+		if ok := errorAs(err, &resourceNotFoundException); ok {
+			return nil, fmt.Errorf("the requested secret %s was not found", secretName)
+		}
+
+		return nil, fmt.Errorf("failed to retrieve secret: %v", err)
+	}
+
+	var credentials AWSCredentials
+	err = json.Unmarshal([]byte(*result.SecretString), &credentials)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal secret: %v", err)
+	}
+
+	return &credentials, nil
+}
+
+func errorAs(err error, target interface{}) bool {
+	if target == nil {
+		return false
+	}
+	return errors.As(err, target)
+}
+
+func CreateRepo() error {
+	secretName := "gotask"
+
+	_, err := getAWSCredentials(secretName)
+	if err != nil {
+		return fmt.Errorf("failed to get AWS credentials: %v", err)
+	}
+
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		return fmt.Errorf("unable to load SDK config, %v", err)
+	}
+
+	svc := ecr.NewFromConfig(cfg)
+
+	repoName := "my-repo"
+	input := &ecr.CreateRepositoryInput{
+		RepositoryName: aws.String(repoName),
+	}
+
+	_, err = svc.CreateRepository(context.Background(), input)
+	if err != nil {
+		var repoAlreadyExistsErr *types.RepositoryAlreadyExistsException
+		if errors.As(err, &repoAlreadyExistsErr) {
+			log.Printf("Repository %s already exists.", repoName)
+			return nil
+		}
+		return fmt.Errorf("failed to create repository: %v", err)
+	}
+
+	log.Printf("Repository %s created successfully.", repoName)
+	return nil
+}
